f35d6c40c122517c7830f36e2796ace07cfb30ce
Rishabh Harod <rishabhharod4449@gmail.com>
2024-07-03 12:03:11 +0530

    add count varible

--- f.rb
+++ f.rb
@@ -1,9 +1,11 @@
 puts "The list of EVEN number from 1 to 100 :-"
-count=0
-for n in 1..100 do
-    if(n%2==0)
-        puts n;
+count = 0
+number = 1
+while number <= 100
+    if(number % 2 == 0)
+        puts number;
         count += 1
     end
+    number += 1;
 end
 puts "total even number is #{count}"
