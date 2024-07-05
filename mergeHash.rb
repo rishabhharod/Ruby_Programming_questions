45dd89074bed249fd115732d5f2a10774b9016a2
Rishabh Harod <rishabhharod4449@gmail.com>
2024-07-05 13:18:59 +0530

    add question in comments

--- mergeHash.rb
+++ mergeHash.rb
@@ -1,15 +1,17 @@
-class Demo 
+#merege the two hash using function 
+class Demo
     def mergeHash(hash1,hash2)
-        newHash = Hash.new()
-        hash1.select do |key,value|
-            newHash[key] = value
-        end
-        hash2.select do |key,value|
-            newHash[key] = value
-        end
-        return newHash
+      newHash = Hash.new()
+      hash1.select do |key,value|
+        newHash[key] = value
+      end
+      hash2.select do |key,value|
+        newHash[key] = value
+      end
+      return newHash
     end
 end
+
 hash1={"JS" => "javascript","PY" => "python","RB" => "ruby","J" => "java"}
 hash2 = {"101" => "Rishabh","102" => "kapil","103" => "Rohit","104" => "Ritika","105" => "Samruddhi"}
 obj = Demo.new()
